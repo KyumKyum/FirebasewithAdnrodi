404e90c507e7a73d4437f540ee6b9e9e831c0935
iraira7777@naver.com <iraira7777@naver.com>
2020-07-11 16:48:12 +0900

    Arrays

--- app/src/main/java/com/example/firestoreinandroid/Note.java
+++ app/src/main/java/com/example/firestoreinandroid/Note.java
@@ -2,21 +2,25 @@ package com.example.firestoreinandroid;
 
 import com.google.firebase.firestore.Exclude;
 
+import java.util.List;
+
 public class Note {
 
     String documentID;
     String title;
     String description;
     int priority;
+    List<String> tags;
 
     public Note() {
         //public no - arg constructor needed.
     }
 
-    public Note(String title, String description, int priority) {
+    public Note(String title, String description, int priority, List<String> tags) {
         this.title = title;
         this.description = description;
         this.priority = priority;
+        this.tags = tags;
     }
 
     @Exclude
@@ -32,17 +36,13 @@ public class Note {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) { this.description = description; }
-
     public int getPriority() { return priority; }
 
-    public void setPriority(int priority) { this.priority = priority; }
+    public List<String> getTags() {
+        return tags;
+    }
 }
